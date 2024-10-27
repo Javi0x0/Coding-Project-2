f303b2efd66537827f62827dce8788a1ac71ca92
Javi <103536638+Javi0x0@users.noreply.github.com>
2024-10-27 16:12:16 -0700

    Update sum_direct.cpp

--- sum_harness_instructional/sum_direct.cpp
+++ sum_harness_instructional/sum_direct.cpp
@@ -11,19 +11,18 @@
 void 
 setup(int64_t N, uint64_t A[])
 {
-
-   printf(" inside direct_sum problem_setup, N=%ld \n", N);
       for (int64_t i = 0; i < N; i++) {
         A[i] = i + 1;
     }
+   printf(" inside direct_sum problem_setup, N=%ld \n", N);
 }
 
 int64_t
 sum(int64_t N, uint64_t A[])
 { 
     int64_t sum = 0;
-for (int i=0; i <= N; i++){
-   sum = N + A[i];
+for (int i=0; i < N; i++){
+   sum += A[i];
    }
    printf(" inside direct_sum perform_sum, N=%ld \n", N);
    
