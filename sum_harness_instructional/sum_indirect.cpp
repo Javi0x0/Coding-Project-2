84b6b9dea49cfc8a3116d5c82c0268f23dabdd4e
Javi <miningwarefare@gmail.com>
2024-10-27 20:13:10 -0700

    im an idiot

--- sum_harness_instructional/sum_indirect.cpp
+++ sum_harness_instructional/sum_indirect.cpp
@@ -10,7 +10,7 @@ void
 setup(int64_t N, uint64_t A[])
 {
      for (int64_t i = 0; i < N; ++i) {
-       A[i] = i % N;
+       A[i] = (i * 2) % N;
    }
    printf(" inside sum_indirect problem_setup, N=%ld \n", N);
 }
@@ -18,12 +18,12 @@ setup(int64_t N, uint64_t A[])
 int64_t
 sum(int64_t N, uint64_t A[])
 {
-int64_t total = 0;
+int64_t sum = 0;
       for (int64_t i = 0; i < N; ++i) {
-       total += A[A[i]];
+       sum += A[A[i]];
    }
    printf(" inside sum_indirect perform_sum, N=%ld \n", N);
 
-   return total;
+   return sum;
 }
 
--- sum_harness_instructional/sum_vector.cpp
+++ sum_harness_instructional/sum_vector.cpp
@@ -20,15 +20,15 @@ setup(int64_t N, uint64_t A[])
 int64_t
 sum(int64_t N, uint64_t A[])
 {
-    int64_t total = 0;
+    int64_t sum = 0;
     int64_t i;
-    for (i = 0; i <= N - 4; i += 4) {
-        total += A[i] + A[i + 1] + A[i + 2] + A[i + 3];
-    }
-    for (; i < N; ++i) {
-        total += A[i];
+    for (i = 0; i < N; i += 2) {
+        sum += A[i];
+    if(i + 1 < N) {
+        sum += A[i+1];
+        }
     }
    printf(" inside sum_vector perform_sum, N=%ld \n", N);
-   return total;
+   return sum;
 }
 
