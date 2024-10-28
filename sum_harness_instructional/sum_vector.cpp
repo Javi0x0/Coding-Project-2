c11f9bec9f896ec4a8688b12f8d698faba599112
Javi <miningwarefare@gmail.com>
2024-10-27 19:58:39 -0700

    im so confused now about a damn vector sum

--- sum_harness_instructional/sum_vector.cpp
+++ sum_harness_instructional/sum_vector.cpp
@@ -20,15 +20,15 @@ setup(int64_t N, uint64_t A[])
 int64_t
 sum(int64_t N, uint64_t A[])
 {
-   int64_t total = 0;
-   for (int64_t i = 0; i < N; ++i) {
-       total += A[i] + A[i + 1] + A[i + 2] + A[i + 3];
-   }
-   for (int64_t i=0; i < N; ++i) {
+    int64_t total = 0;
+    int64_t i;
+    for (i = 0; i <= N - 4; i += 4) {
+        total += A[i] + A[i + 1] + A[i + 2] + A[i + 3];
+    }
+    for (; i < N; ++i) {
         total += A[i];
     }
    printf(" inside sum_vector perform_sum, N=%ld \n", N);
-
    return total;
 }
 
